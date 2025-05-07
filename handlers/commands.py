c4c1ebb8ebc225f1d28fdc2a2f01d4d8179678f5
Mirali-Shadow <amiralimhmdi1386@gmail.com>
2025-05-07 22:20:58 +0330

    Update command to aiogram 3

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,8 +1,9 @@
 from aiogram import types
 import utils.db_api.db
 from dispatcher import dp
+from aiogram.filters import CommandStart
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(CommandStart())
 async def start_command(message: types.Message):
     await message.answer(await utils.db_api.db.commands.get_rows())
